89d6cc490fb9e4dd4bff3e385a11a6e04e40c060
T8RIN <oolbp@bk.ru>
2022-08-12 01:13:11 +0300

    Chat bubble adaptive position for time added

--- app/src/main/java/ru/tech/cookhelper/presentation/chat/components/ChatBubbleItem.kt
+++ app/src/main/java/ru/tech/cookhelper/presentation/chat/components/ChatBubbleItem.kt
@@ -6,7 +6,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,28 +42,63 @@ fun ChatBubbleItem(user: User?, message: Message) {
     val timePadding = if (myMessage) PaddingValues(4.dp)
     else PaddingValues(top = 4.dp, start = 4.dp, bottom = 4.dp, end = 8.dp)
 
+    var placeTimeUnderTheText by remember { mutableStateOf(false) }
+
     var maxWidth by remember { mutableStateOf(0.dp) }
     val density = LocalDensity.current
 
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .onSizeChanged {
-                maxWidth = with(density) { it.width.toDp() * 0.85f }
+            .onGloballyPositioned {
+                maxWidth = with(density) { it.size.width.toDp() * 0.85f }
             }
             .padding(bubblePadding),
         horizontalArrangement = horizontalArrangement
     ) {
-        Card(colors = CardDefaults.cardColors(containerColor = color), shape = shape, modifier = Modifier.widthIn(max = maxWidth)) {
-            Row(verticalAlignment = Alignment.Bottom) {
-                Text(text = message.text, modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp))
-                Text(
-                    text = message.timestamp.toDate(),
-                    fontSize = 12.sp,
-                    modifier = Modifier.padding(timePadding),
-                    color = contentColorFor(color).copy(alpha = 0.5f),
-                    maxLines = 1,
-                )
+        Card(
+            colors = CardDefaults.cardColors(containerColor = color),
+            shape = shape,
+            modifier = Modifier
+                .widthIn(max = maxWidth)
+                .onGloballyPositioned {
+                    val currentWidth = with(density) { it.size.width.toDp() }
+                    if (currentWidth == maxWidth) {
+                        placeTimeUnderTheText = true
+                    }
+                }
+        ) {
+            if (!placeTimeUnderTheText) {
+                Row(verticalAlignment = Alignment.Bottom) {
+                    Text(
+                        text = message.text,
+                        modifier = Modifier
+                            .padding(start = 16.dp, top = 8.dp, bottom = 8.dp, end = 8.dp)
+                            .weight(1f, false)
+                    )
+                    Text(
+                        text = message.timestamp.toDate(),
+                        fontSize = 12.sp,
+                        modifier = Modifier.padding(timePadding),
+                        color = contentColorFor(color).copy(alpha = 0.5f),
+                        maxLines = 1,
+                    )
+                }
+            } else {
+                Column(horizontalAlignment = Alignment.End) {
+                    Text(
+                        text = message.text,
+                        modifier = Modifier
+                            .padding(start = 16.dp, top = 8.dp, end = 8.dp, bottom = 4.dp)
+                    )
+                    Text(
+                        text = message.timestamp.toDate(),
+                        fontSize = 12.sp,
+                        modifier = Modifier.padding(timePadding),
+                        color = contentColorFor(color).copy(alpha = 0.5f),
+                        maxLines = 1,
+                    )
+                }
             }
         }
     }
