1109fcf82cecee13be3ecdfc7e67a36210d85363
T8RIN <oolbp@bk.ru>
2022-08-16 23:12:07 +0300

    removed unused code

--- app/src/main/java/ru/tech/cookhelper/data/local/CookHelperDatabase.kt
+++ app/src/main/java/ru/tech/cookhelper/data/local/CookHelperDatabase.kt
@@ -2,23 +2,17 @@ package ru.tech.cookhelper.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import ru.tech.cookhelper.data.local.dao.FavRecipeDao
-import ru.tech.cookhelper.data.local.dao.FridgeDao
 import ru.tech.cookhelper.data.local.dao.SettingsDao
 import ru.tech.cookhelper.data.local.dao.UserDao
-import ru.tech.cookhelper.data.local.entity.FavRecipeEntity
-import ru.tech.cookhelper.data.local.entity.ProductEntity
 import ru.tech.cookhelper.data.local.entity.SettingsEntity
 import ru.tech.cookhelper.data.local.entity.UserEntity
 
 @Database(
-    entities = [FavRecipeEntity::class, ProductEntity::class, SettingsEntity::class, UserEntity::class],
+    entities = [SettingsEntity::class, UserEntity::class],
     exportSchema = false,
     version = 1
 )
 abstract class CookHelperDatabase : RoomDatabase() {
-    abstract val favRecipeDao: FavRecipeDao
-    abstract val fridgeDao: FridgeDao
     abstract val settingsDao: SettingsDao
     abstract val userDao: UserDao
 }
--- app/src/main/java/ru/tech/cookhelper/data/local/entity/ProductEntity.kt
+++
@@ -1,9 +0,0 @@
-package ru.tech.cookhelper.data.local.entity
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
-data class ProductEntity(
-    @PrimaryKey val id: Int
-)
