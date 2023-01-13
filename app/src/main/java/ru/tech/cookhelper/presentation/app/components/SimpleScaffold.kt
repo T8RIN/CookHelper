72b84f1282f6960ca28c0ededddfbb5596a45e86
T8RIN <oolbp@bk.ru>
2023-01-13 14:04:44 +0300

    fixed bug

--- app/src/main/java/ru/tech/cookhelper/presentation/app/components/SimpleScaffold.kt
+++ app/src/main/java/ru/tech/cookhelper/presentation/app/components/SimpleScaffold.kt
@@ -1,62 +1,21 @@
 package ru.tech.cookhelper.presentation.app.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun SimpleScaffold(
     modifier: Modifier = Modifier,
     topAppBar: (@Composable () -> Unit)? = null,
     bottomAppBar: (@Composable () -> Unit)? = null,
-    autoApplyPadding: Boolean = true,
-    content: @Composable (PaddingValues?) -> Unit
+    content: @Composable () -> Unit
 ) {
-    val density = LocalDensity.current
-
-    var topAppBarHeight by remember { mutableStateOf(0.dp) }
-    var bottomAppBarHeight by remember { mutableStateOf(0.dp) }
-
-    val paddingValues = remember(topAppBarHeight, bottomAppBarHeight) {
-        PaddingValues(top = topAppBarHeight, bottom = bottomAppBarHeight)
-    }
-
-    Box(
+    Column(
         modifier = modifier,
     ) {
-        Box(
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .onGloballyPositioned {
-                    with(density) {
-                        topAppBarHeight = it.size.height.toDp()
-                    }
-                },
-            content = {
-                topAppBar?.invoke()
-            }
-        )
-        Box(modifier = if (autoApplyPadding) Modifier.padding(paddingValues) else Modifier) {
-            content(if (autoApplyPadding) null else paddingValues)
-        }
-        Box(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .onGloballyPositioned {
-                    with(density) {
-                        bottomAppBarHeight = it.size.height.toDp()
-                    }
-                },
-            content = {
-                bottomAppBar?.invoke()
-            }
-        )
-
+        topAppBar?.invoke()
+        content()
+        bottomAppBar?.invoke()
     }
 }
