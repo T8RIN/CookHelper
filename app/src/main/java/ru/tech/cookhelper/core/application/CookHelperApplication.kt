3ae30cd184be10c71c3d16c839e4ea2d512f2eb0
T8RIN <oolbp@bk.ru>
2023-02-10 08:20:02 +0300

    fix

--- app/src/main/java/ru/tech/cookhelper/core/application/CookHelperApplication.kt
+++ app/src/main/java/ru/tech/cookhelper/core/application/CookHelperApplication.kt
@@ -1,13 +1,7 @@
 package ru.tech.cookhelper.core.application
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CookHelperApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-    }
-}
+class CookHelperApplication : Application()
